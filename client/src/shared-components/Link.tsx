8a7c44f8b2e80f7d4e8275c76564938d8a927049
Oleksandr Sakun <sakun.oleks@gmail.com>
2019-05-20 21:55:20 +0300

    :sparkles: Implement shared Link component

---
+++ client/src/shared-components/Link.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import MuiLink from "@material-ui/core/Link";
+import {Link as RouterLink} from "react-router-dom";
+
+interface ILinkProps {
+  to: string;
+  children: any;
+}
+
+export const Link = ({ to, children }: ILinkProps) => (
+  <MuiLink
+    color="inherit"
+    underline="none"
+    component={(props) => (
+      <RouterLink to={to} {...props} >{children}</RouterLink>
+    )}
+  />
+);
